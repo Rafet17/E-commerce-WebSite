650c9302c40310db2746f43a75bab7b3e3bee99a
Rafet17 <rafet.hasanov17@gmail.com>
2024-06-30 14:28:13 +0400

    button disabled

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 import headerFunc from "./header.js";
 import productFunc from "./product.js";
 
-//! add product to localStorage
+//! add product to localStorage start
 
-async function getData(){
+(async function getData(){
     const photos = await fetch("./js/data.json");
     const data = await photos.json();
    
 
    data ? localStorage.setItem("products",JSON.stringify(data)) : [];   
-}
+   productFunc();
+})();
 
-getData();
+//! add product to localStorage start
 
 
+
+
+const cartItems = document.querySelector(".header-cart-count");
+
+cartItems.innerHTML = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")).length:"0";

b43401a3c6e0a248e2877dc9109cfa2f35691087
Rafet17 <rafet.hasanov17@gmail.com>
2024-07-06 15:31:55 +0400

    single product thubnails, single product zoom, active classes

--- js/single-product.js
+++ js/single-product.js
@@ -1,3 +1,10 @@
+
+import { thumbsActiveFunc } from "./single-product/thumbs-active.js";
+import { singleThumbs } from "./glide.js";
+import zoomFunc from "./single-product/zoom.js";
+import colorsFunc from "./single-product/colors.js";
+import valuesFunc from "./single-product/values.js";
+
 const productId = localStorage.getItem("productId")
     ? JSON.parse(localStorage.getItem("productId"))
     : localStorage.setItem("producId", JSON.stringify(1));
@@ -6,25 +13,43 @@ const products = localStorage.getItem("products")
     ? JSON.parse(localStorage.getItem("products"))
     : localStorage.setItem("producs", JSON.stringify([]));
 
-const findProduct =products.find((item)=> item.id === Number(productId));
+const findProduct = products.find((item) => item.id === Number(productId));
+
 
 
 /* product title */
 
 const productTitle = document.querySelector(".product-title")
 
-productTitle.innerHTML =findProduct.name;
+productTitle.innerHTML = findProduct.name;
 
 /* product price */
 
-const newPriceDOM =document.querySelector(".new-price");
-const oldPriceDOM =document.querySelector(".old-price");
+const newPriceDOM = document.querySelector(".new-price");
+const oldPriceDOM = document.querySelector(".old-price");
 
 newPriceDOM.innerHTML = findProduct.price.newPrice.toFixed(2);
 oldPriceDOM.innerHTML = findProduct.price.oldPrice.toFixed(2);
 
 /* product galery */
 
-const singleImageDOM =document.querySelector("#single-image");
+const singleImageDOM = document.querySelector("#single-image");
 
 singleImageDOM.src = findProduct.img.singleImage;
+
+const galleryThumbs = document.querySelector(".gallery-thumbs")
+let result = "";
+findProduct.img.thumbs.forEach((item) => {
+    result += `
+ <li class="glide__slide">
+   <img src=${item} class="img-fluid" alt="">
+ </li>`
+});
+galleryThumbs.innerHTML =result;
+singleThumbs();
+thumbsActiveFunc();
+
+const productThumbs = document.querySelectorAll(".product-thumb .glide__slide img");
+
+productThumbs[0].classList.add("active");
+

db96b211641b08a76b69db1fc11f93c1c4436476
Rafet17 <rafet.hasanov17@gmail.com>
2024-07-09 12:33:14 +0400

    add to cart, tabs

--- js/single-product.js
+++ js/single-product.js
@@ -4,6 +4,7 @@ import { singleThumbs } from "./glide.js";
 import zoomFunc from "./single-product/zoom.js";
 import colorsFunc from "./single-product/colors.js";
 import valuesFunc from "./single-product/values.js";
+import tabsFunc from "./single-product/tabs.js";
 
 const productId = localStorage.getItem("productId")
     ? JSON.parse(localStorage.getItem("productId"))
@@ -52,4 +53,27 @@ thumbsActiveFunc();
 const productThumbs = document.querySelectorAll(".product-thumb .glide__slide img");
 
 productThumbs[0].classList.add("active");
+
+/! add to cart /
+
+let cart = localStorage.getItem("cart") 
+? JSON.parse(localStorage.getItem("cart") )
+:[];
+
+const findCart = cart.find((item)=> item.id === findProduct.id);
+const btnAddToCart = document.getElementById("add-to cart");
+const quantityDOM = document.getElementById("quantity");
+let cartItems = document.querySelector(".header-cart-count")
+
+if(findCart){
+    btnAddToCart.setAttribute("disabled", "disabled");
+}
+else{
+    btnAddToCart.addEventListener("click", function(){
+        cart.push({...findProduct, quantity: Number(quantityDOM.value)});
+        localStorage.setItem("cart", JSON.stringify(cart));
+        cartItems.innerHTML =cart.lenght;
+    });
+};
+
 

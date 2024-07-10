96715f68bd25ea7bfe527e4d8fa885d03b134439
Rafet17 <rafet.hasanov17@gmail.com>
2024-07-10 12:59:19 +0400

    search product list, click the search product, search product

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 
 import headerFunc from "./header.js";
 import productFunc from "./product.js";
+import searchFunc from "./search.js";
 
 //! add product to localStorage start
-
-(async function getData(){
-    const photos = await fetch("./js/data.json");
+(async function () {
+    const photos = await fetch("../js/data.json");
     const data = await photos.json();
-   
-
-   data ? localStorage.setItem("products",JSON.stringify(data)) : [];   
-   productFunc();
-})();
-
-//! add product to localStorage start
-
-
-
-
-const cartItems = document.querySelector(".header-cart-count");
-
-cartItems.innerHTML = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")).length:"0";
+  
+    data ? localStorage.setItem("product", JSON.stringify(data)) : [];
+    productFunc();
+    searchFunc(data);
+  })();
+  //! add product to localStorage end
+  
+  //! add cartItems to localStorage start
+  const cartItems = document.querySelector(".header-cart-count");
+  
+  cartItems.innerHTML = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart")).length
+    : "0";
+  //! add cartItems to localStorage end
+  
--- js/single-product.js
+++ js/single-product.js
@@ -1,79 +1,76 @@
-
 import { thumbsActiveFunc } from "./single-product/thumbs-active.js";
 import { singleThumbs } from "./glide.js";
 import zoomFunc from "./single-product/zoom.js";
 import colorsFunc from "./single-product/colors.js";
 import valuesFunc from "./single-product/values.js";
 import tabsFunc from "./single-product/tabs.js";
+import commentsFunc from "./single-product/comment.js";
 
 const productId = localStorage.getItem("productId")
-    ? JSON.parse(localStorage.getItem("productId"))
-    : localStorage.setItem("producId", JSON.stringify(1));
+  ? JSON.parse(localStorage.getItem("productId"))
+  : localStorage.setItem("productId", JSON.stringify(1));
 
 const products = localStorage.getItem("products")
-    ? JSON.parse(localStorage.getItem("products"))
-    : localStorage.setItem("producs", JSON.stringify([]));
+  ? JSON.parse(localStorage.getItem("products"))
+  : localStorage.setItem("products", JSON.stringify([]));
 
 const findProduct = products.find((item) => item.id === Number(productId));
 
-
-
-/* product title */
-
-const productTitle = document.querySelector(".product-title")
+//! product title
+const productTitle = document.querySelector(".product-title");
 
 productTitle.innerHTML = findProduct.name;
 
-/* product price */
-
+//! product price
 const newPriceDOM = document.querySelector(".new-price");
 const oldPriceDOM = document.querySelector(".old-price");
 
-newPriceDOM.innerHTML = findProduct.price.newPrice.toFixed(2);
-oldPriceDOM.innerHTML = findProduct.price.oldPrice.toFixed(2);
+newPriceDOM.innerHTML = `$${findProduct.price.newPrice.toFixed(2)}`;
+oldPriceDOM.innerHTML = `$${findProduct.price.oldPrice.toFixed(2)}`;
 
-/* product galery */
+//! product gallery
 
 const singleImageDOM = document.querySelector("#single-image");
 
 singleImageDOM.src = findProduct.img.singleImage;
 
-const galleryThumbs = document.querySelector(".gallery-thumbs")
+const galleryThumbs = document.querySelector(".gallery-thumbs");
 let result = "";
 findProduct.img.thumbs.forEach((item) => {
-    result += `
- <li class="glide__slide">
-   <img src=${item} class="img-fluid" alt="">
- </li>`
+  result += `
+  <li class="glide__slide">
+   <img src=${item} alt="" class="img-fluid">
+  </li>
+  `;
 });
-galleryThumbs.innerHTML =result;
+
+galleryThumbs.innerHTML = result;
 singleThumbs();
 thumbsActiveFunc();
 
-const productThumbs = document.querySelectorAll(".product-thumb .glide__slide img");
+const productThumbs = document.querySelectorAll(
+  ".product-thumb .glide__slide img"
+);
 
 productThumbs[0].classList.add("active");
 
-/! add to cart /
+//! add to cart
+let cart = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
 
-let cart = localStorage.getItem("cart") 
-? JSON.parse(localStorage.getItem("cart") )
-:[];
-
-const findCart = cart.find((item)=> item.id === findProduct.id);
-const btnAddToCart = document.getElementById("add-to cart");
+const findCart = cart.find((item) => item.id === findProduct.id);
+const btnAddToCart = document.getElementById("add-to-cart");
 const quantityDOM = document.getElementById("quantity");
-let cartItems = document.querySelector(".header-cart-count")
+let cartItems = document.querySelector(".header-cart-count");
 
-if(findCart){
+if (findCart) {
+  btnAddToCart.setAttribute("disabled", "disabled");
+} else {
+  btnAddToCart.addEventListener("click", function () {
+    cart.push({ ...findProduct, quantity: Number(quantityDOM.value) });
     btnAddToCart.setAttribute("disabled", "disabled");
+    localStorage.setItem("cart", JSON.stringify(cart));
+    cartItems.innerHTML = cart.length;
+  });
 }
-else{
-    btnAddToCart.addEventListener("click", function(){
-        cart.push({...findProduct, quantity: Number(quantityDOM.value)});
-        localStorage.setItem("cart", JSON.stringify(cart));
-        cartItems.innerHTML =cart.lenght;
-    });
-};
-
-
